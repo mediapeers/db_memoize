629a60a9bae56e1e4202a34056ca1f1ce7f80cf8
johannes-kostas goetzinger <goetzinger@mediapeers.com>
2016-12-01 10:30:48 +0000

    loads warmup task into rails; set custom key by default to db schema version

--- lib/db_memoize/railtie.rb
+++ lib/db_memoize/railtie.rb
@@ -2,10 +2,14 @@ module DbMemoize
   class Railtie < ::Rails::Railtie
     initializer 'Rails logger' do
       DbMemoize.logger = Rails.logger
+
+      # by default invalidate cache when db schema has changed
+      DbMemoize.default_custom_key = ActiveRecord::Migrator.current_version
     end
 
     rake_tasks do
       load 'tasks/clear.rake'
+      load 'tasks/warmup.rake'
     end
   end
 end
