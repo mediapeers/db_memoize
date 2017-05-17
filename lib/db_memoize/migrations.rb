2489c27841260da0ada84ee6ed42e98bd54b7909
eno <eno@open-lab.org>
2017-05-17 17:28:13 +0200

    Adds stronger SQL-leven constraints

--- lib/db_memoize/migrations.rb
+++ lib/db_memoize/migrations.rb
@@ -3,12 +3,12 @@ module DbMemoize
     class << self
       def create_tables(migration)
         migration.create_table :memoized_values, id: false do |t|
-          t.string :entity_table_name
-          t.integer :entity_id
-          t.string :method_name
+          t.string :entity_table_name, null: false
+          t.integer :entity_id, null: false
+          t.string :method_name, null: false
           t.string :arguments_hash
           t.binary :value
-          t.datetime :created_at
+          t.datetime :created_at, null: false
         end
 
         migration.add_index :memoized_values, [:entity_table_name, :entity_id]
