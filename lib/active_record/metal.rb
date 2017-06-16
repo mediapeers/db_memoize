615a605ee32941294127689daf69f2ab850d0040
eno <eno@open-lab.org>
2017-06-16 12:01:51 +0000

    Performance improvements

--- lib/active_record/metal.rb
+++ lib/active_record/metal.rb
@@ -12,21 +12,27 @@ module ActiveRecord
     end
 
     class Adapter
-      ColumnInfo = Struct.new(:column, :type) unless defined?(ColumnInfo)
+      class PkInfo
+        attr_reader :column, :type
+
+        def initialize(base_klass)
+          @column = base_klass.primary_key
+          @type   = @column && base_klass.columns_hash.fetch(@column).type
+        end
+      end
 
       def initialize(base_klass)
         @base_klass  = base_klass
-
-        # setup primary key information. This is necessary to allow the create! method
-        # to return the primary key of a newly created entry.
-        pk_column    = @base_klass.primary_key
-        pk_type      = @base_klass.columns_hash.fetch(pk_column).type if pk_column
-        @primary_key = ColumnInfo.new(pk_column, pk_type)
+        @query_cache = {}
       end
 
-      attr_reader :primary_key
-
       private
+
+      # setup primary key information. This is necessary to allow the create! method
+      # to return the primary key of a newly created entry.
+      def primary_key
+        @primary_key ||= PkInfo.new(@base_klass)
+      end
 
       def table_name
         @base_klass.table_name
@@ -40,15 +46,22 @@ module ActiveRecord
         @base_klass.columns_hash.key?(column_name)
       end
 
-      def sql_cache
-        @sql_cache ||= {}
+      def insert_sql(field_names)
+        @query_cache[field_names] ||= _insert_sql(field_names)
       end
 
-      def insert_sql(field_names)
-        sql_cache[[:insert_sql, field_names]] ||= _insert_sql(field_names)
+      DATABASE_IDENTIFIER_REGEX = /\A\w+\z/
+
+      def check_database_identifiers!(*strings)
+        strings.each do |s|
+          return if DATABASE_IDENTIFIER_REGEX =~ s.to_s
+          raise ArgumentError, "Invalid database identifier: #{s.inspect}"
+        end
       end
 
       def _insert_sql(field_names)
+        check_database_identifiers! table_name, *field_names
+
         placeholders = 0.upto(field_names.count - 1).map { |idx| "$#{idx + 1}" }
 
         if column?('created_at')
