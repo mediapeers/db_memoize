482673186a83d1233243b0d990f684a80ceffdab
eno <eno@open-lab.org>
2018-10-08 18:03:28 +0100

    Fixes migration

--- lib/db_memoize/migrations.rb
+++ lib/db_memoize/migrations.rb
@@ -2,48 +2,32 @@ module DbMemoize
   class Migrations
     class << self
       def create_tables(migration)
-        migration.create_table :memoized_values, id: false do |t|
-          t.string :entity_table_name, null: false
-          t.integer :entity_id, null: false
-          t.string :method_name, null: false
-          t.string :arguments_hash
-          t.binary :value
-          t.datetime :created_at, null: false
-        end
+        migration.execute <<~SQL
+          CREATE TABLE IF NOT EXISTS memoized_values (
+            entity_table_name varchar,
+            entity_id integer,
+            method_name varchar,
+            created_at timestamp without time zone
+          );
 
-        migration.add_index :memoized_values, [:entity_table_name, :entity_id]
-        migrate_empty_arguments_support(migration)
-        drop_arguments_support(migration)
-        add_discrete_value_columns(migration)
-      end
+          -- entity_id/entity_table_name should have a better chance to be useful, since
+          -- there is more variance in entity_ids than there is in entity_table_names.
+          DROP INDEX IF EXISTS index_memoized_values_on_entity_id_and_entity_table_name;
+          DROP INDEX IF EXISTS index_memoized_values_on_entity_table_name_and_entity_id;
+          CREATE UNIQUE INDEX IF NOT EXISTS memoized_attributes_idx2
+            ON memoized_values(entity_id, entity_table_name, method_name);
 
-      def migrate_empty_arguments_support(migration)
-        # entity_id/entity_table_name should have a better chance to be useful, since
-        # there is more variance in entity_ids than there is in entity_table_names.
-        migration.remove_index :memoized_values, [:entity_table_name, :entity_id]
-        migration.add_index :memoized_values, [:entity_id, :entity_table_name]
+          ALTER TABLE memoized_values DROP COLUMN IF EXISTS arguments_hash;
+          ALTER TABLE memoized_values DROP COLUMN IF EXISTS value;
 
-        # add an index to be useful to look up entries where arguments_hash is NULL.
-        # (which is the case for plain attributes of an object)
-        migration.execute 'CREATE INDEX IF NOT EXISTS memoized_attributes_idx ON memoized_values((arguments_hash IS NULL))'
-      end
-
-      def drop_arguments_support(migration)
-        migration.execute 'DROP INDEX IF EXISTS memoized_attributes_idx'
-        migration.execute 'ALTER TABLE memoized_values DROP COLUMN IF EXISTS arguments_hash'
-
-        migration.remove_index :memoized_values, [:entity_id, :entity_table_name]
-        migration.add_index :memoized_values, [:entity_id, :entity_table_name, :method_name], unique: true, name: 'memoized_attributes_idx2'
-      end
-
-      def add_discrete_value_columns(migration)
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_string varchar'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_integer bigint'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_float double precision'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_time timestamp without time zone'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_boolean boolean'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_object jsonb'
-        migration.execute 'ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_nil boolean'
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_string varchar;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_integer bigint;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_float double precision;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_time timestamp without time zone;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_object jsonb;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_boolean boolean;
+          ALTER TABLE memoized_values ADD COLUMN IF NOT EXISTS val_nil boolean;
+        SQL
       end
     end
   end
