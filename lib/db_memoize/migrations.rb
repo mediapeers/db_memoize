d094222c15d87edb07dd07e86984b980b67c9ccc
eno <eno@open-lab.org>
2018-10-24 11:20:43 +0200

    Cleaned up migration

--- lib/db_memoize/migrations.rb
+++ lib/db_memoize/migrations.rb
@@ -7,28 +7,23 @@ module DbMemoize
 
           CREATE TABLE IF NOT EXISTS db_memoize.memoized_values (
             entity_table_name varchar NOT NULL,
-            entity_id integer NOT NULL,
-            method_name varchar NOT NULL,
-            created_at timestamp without time zone NOT NULL
+            entity_id         integer NOT NULL,
+            method_name       varchar NOT NULL,
+
+            val_string        varchar,
+            val_integer       bigint,
+            val_float         double precision,
+            val_time          timestamp without time zone,
+            val_object        jsonb,
+            val_boolean       boolean,
+            val_nil           boolean,
+            created_at        timestamp without time zone NOT NULL
           );
 
           -- entity_id/entity_table_name should have a better chance to be useful, since
           -- there is more variance in entity_ids than there is in entity_table_names.
-          DROP INDEX IF EXISTS index_memoized_values_on_entity_id_and_entity_table_name;
-          DROP INDEX IF EXISTS index_memoized_values_on_entity_table_name_and_entity_id;
           CREATE UNIQUE INDEX IF NOT EXISTS memoized_attributes_idx2
             ON db_memoize.memoized_values(entity_id, entity_table_name, method_name);
-
-          ALTER TABLE db_memoize.memoized_values DROP COLUMN IF EXISTS arguments_hash;
-          ALTER TABLE db_memoize.memoized_values DROP COLUMN IF EXISTS value;
-
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_string varchar;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_integer bigint;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_float double precision;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_time timestamp without time zone;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_object jsonb;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_boolean boolean;
-          ALTER TABLE db_memoize.memoized_values ADD COLUMN IF NOT EXISTS val_nil boolean;
         SQL
       end
     end
